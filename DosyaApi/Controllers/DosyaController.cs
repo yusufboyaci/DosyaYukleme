30e63311a8f320ba88672fdcc28853372021c495
Yusuf Boyacı <85557228+yusufboyaci@users.noreply.github.com>
2022-05-19 23:09:41 +0300

    Add project files.

---
+++ DosyaApi/Controllers/DosyaController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DosyaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DosyaController : Controller
+    {
+        private readonly IWebHostEnvironment _env;
+        public DosyaController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+        [HttpPost("UploadFile")]
+        public IActionResult UploadFile()
+        {
+            foreach (var item in Request.Form.Files)
+            {
+                string fullPath = Path.Combine(_env.ContentRootPath, "wwwroot\\files", item.FileName);
+                using(FileStream fs = System.IO.File.Create(fullPath))
+                {
+                    item.CopyTo(fs);
+                    fs.Flush();
+                }
+                return Json("Upload image successfully");
+            }
+            return Json("Please Try Again!!");
+        }
+    }
+}
